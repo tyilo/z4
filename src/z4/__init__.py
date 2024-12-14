fd4f3c91e273625bc7f0ef987e0cfc9f79348cc2
Asger Hautop Drewsen <asger@tyilo.com>
2024-12-14 19:09:27 +0100

    Enable more ruff lints

--- src/z4/__init__.py
+++ src/z4/__init__.py
@@ -1,10 +1,19 @@
-from z3 import *  # noqa
+"""z3 with some improvements."""
+
+from z3 import *  # noqa: F403, I001
+
+
+from collections.abc import Iterable, Callable
+from typing import Any
 from z3 import (
     Abs,
     And,
     ArithRef,
     BitVec,
     BitVecRef,
+    BoolRef,
+    Context,
+    ExprRef,
     Extract,
     If,
     LShR,
@@ -18,48 +27,66 @@ from z3 import (
 )
 
 
-class Z3SolveException(Exception):
-    pass
+class Z3SolveError(Exception):
+    """A Z3 error."""
 
 
-class Z3Unsat(Z3SolveException):
-    pass
+class Z3UnsatError(Z3SolveError):
+    """Z3 has proven that the constraints cannot be satisfied."""
 
 
-class Z3Unknown(Z3SolveException):
-    pass
+class Z3UnknownError(Z3SolveError):
+    """Z3 doesn't know whether the constraints can be satisfied."""
 
 
-class Z3CounterExample(Z3SolveException):
+class Z3CounterExampleError(Z3SolveError):
+    """A counterexample to the theorem was found."""
+
     model: ModelRef
 
-    def __init__(self, model: ModelRef):
+    def __init__(self, model: ModelRef) -> None:
+        """Initialize from a model containing the counterexample."""
         super().__init__(model)
         self.model = model
 
 
-class Z3Unbounded(Z3SolveException):
+class Z3UnboundedError(Z3SolveError):
+    """The objective function is unbounded."""
+
     model: ModelRef
 
-    def __init__(self, model: ModelRef):
+    def __init__(self, model: ModelRef) -> None:
+        """Initialize from a feasible model."""
         super().__init__(model)
         self.model = model
 
 
-def easy_solve(constraints):
+def easy_solve(constraints: Iterable[BoolRef]) -> ModelRef:
+    """
+    Try to find a model satisfying the constraints.
+
+    :raises Z3UnsatError: if the constraints can't be satisfied
+    :raises Z3UnknownError: if Z3 can't determine whether the constraints
+                            can be satisfied
+    """
     solver = Solver()
     solver.add(*constraints)
     res = solver.check()
     if res == unsat:
-        raise Z3Unsat
-    elif res == unknown:
-        raise Z3Unknown
+        raise Z3UnsatError
+    if res == unknown:
+        raise Z3UnknownError
 
     return solver.model()
 
 
-def find_all_solutions(constraints):
+def find_all_solutions(constraints: Iterable[BoolRef]) -> Iterable[ModelRef]:
     """
+    Find all models satisfying the constraints.
+
+    :raises Z3UnknownError: if Z3 can't determine whether the constraints
+                            can be satisfied
+
     >>> def normalize_result(r):
     ...     return sorted(sorted((f.name(), m[f].as_long()) for f in m) for m in r)
     >>> a, b, c = Ints("a b c")
@@ -73,7 +100,7 @@ def find_all_solutions(constraints):
     while True:
         res = solver.check()
         if res == unknown:
-            raise Z3Unknown
+            raise Z3UnknownError
         elif res == unsat:
             return
 
@@ -83,42 +110,85 @@ def find_all_solutions(constraints):
         solver.add(Not(And([f() == model[f] for f in model if f.arity() == 0])))
 
 
-def easy_prove(claim):
+def easy_prove(claim: BoolRef) -> bool:
+    """
+    Prove a claim.
+
+    :return Always True
+    :raises Z3UnknownError: if Z3 can't determine whether the claim
+                            is true or not
+    :raises Z3CounterExamplError: if Z3 found a counterexample to the claim
+    """
     solver = Solver()
     solver.add(Not(claim))
     res = solver.check()
     if res == unknown:
-        raise Z3Unknown
-    elif res == sat:
-        raise Z3CounterExample(solver.model())
-    else:
-        return True
+        raise Z3UnknownError
+    if res == sat:
+        raise Z3CounterExampleError(solver.model())
+
+    return True
 
 
-def _optimize(cost, constraints, dir_f, obj_f):
+def _optimize(
+    cost: ArithRef,
+    constraints: Iterable[BoolRef],
+    dir_f: Callable[[Optimize, ArithRef], ExprRef],
+    obj_f: Callable[[Optimize, ExprRef], ArithRef],
+) -> tuple[ArithRef, ModelRef]:
     opt = Optimize()
     opt.add(*constraints)
     objective = dir_f(opt, cost)
     res = opt.check()
     if res == unsat:
-        raise Z3Unsat
-    elif res == unknown:
-        raise Z3Unknown
+        raise Z3UnsatError
+    if res == unknown:
+        raise Z3UnknownError
 
     model = opt.model()
     value = obj_f(opt, objective)
     if type(value) is ArithRef and str(value) in {"oo", "-1*oo"}:
-        raise Z3Unbounded(model)
+        raise Z3UnboundedError(model)
 
     return value, model
 
 
-def maximize(cost, constraints):
-    return _optimize(cost, constraints, Optimize.maximize, Optimize.upper)
+def maximize(
+    cost: ArithRef, constraints: Iterable[BoolRef]
+) -> tuple[ArithRef, ModelRef]:
+    """
+    Maximize :cost while satisfying :constraints.
+
+    :return (max_value, model)
+    :raises Z3UnsatError: if the constraints can't be satisfied
+    :raises Z3UnknownError: if Z3 can't determine whether the constraints
+                            can be satisfied
+    """
+    return _optimize(
+        cost,
+        constraints,
+        Optimize.maximize,
+        Optimize.upper,
+    )
 
 
-def minimize(cost, constraints):
-    return _optimize(cost, constraints, Optimize.minimize, Optimize.lower)
+def minimize(
+    cost: ArithRef, constraints: Iterable[BoolRef]
+) -> tuple[ArithRef, ModelRef]:
+    """
+    Minimize :cost while satisfying :constraints.
+
+    :return (min_value, model)
+    :raises Z3UnsatError: if the constraints can't be satisfied
+    :raises Z3UnknownError: if Z3 can't determine whether the constraints
+                            can be satisfied
+    """
+    return _optimize(
+        cost,
+        constraints,
+        Optimize.minimize,
+        Optimize.lower,
+    )
 
 
 BitVecRef.__rshift__ = LShR
@@ -126,17 +196,27 @@ BitVecRef.__rrshift__ = lambda a, b: LShR(b, a)
 
 
 class ByteVec(BitVecRef):
-    def __init__(self, name, byte_count, ctx=None):
+    """Byte-vector representation of a Z3 bit-vector."""
+
+    def __init__(self, name: str, byte_count: int, ctx: Context | None = None) -> None:
+        """Create a new byte-vector with name and number of bytes."""
         self.byte_count = byte_count
         self.bv = BitVec(name, byte_count * 8, ctx)
 
-    def __getattr__(self, attr):
+    def __getattr__(self, attr: str) -> Any:  # noqa: ANN401
+        """Get the attribute of the underlying BitVecRef."""
         return getattr(self.bv, attr)
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """Byte count."""
         return self.byte_count
 
-    def __getitem__(self, i):
+    def __getitem__(self, i: Any) -> BitVecRef:  # noqa: ANN401
+        """
+        I'th byte as a BitVecRef.
+
+        Supports negative indices for indexing from the back.
+        """
         if not isinstance(i, int):
             raise TypeError
 
@@ -148,22 +228,25 @@ class ByteVec(BitVecRef):
 
         return Extract(8 * i + 7, 8 * i, self.bv)
 
-    def value(self, model):
+    def value(self, model: ModelRef) -> None:
+        """Byte value of this in the given model."""
         v = model[self.bv].as_long()
         return v.to_bytes(self.byte_count, "little")
 
 
-def BoolToInt(x):
+def BoolToInt(x: BoolRef) -> ArithRef:  # noqa: N802
+    """Map a Z3 bool to an int."""
     return If(x, 1, 0)
 
 
-def Sgn(x):
+def Sgn(x: ArithRef) -> ArithRef:  # noqa: N802
+    """Sign of x."""
     return If(x == 0, 0, If(x > 0, 1, -1))
 
 
-def TruncDiv(a, b):
+def TruncDiv(a: ArithRef, b: ArithRef) -> ArithRef:  # noqa: N802
     """
-    Truncated division, a / b rounded towards zero.
+    Calculate a / b rounded towards zero.
 
     >>> a, b, c = Ints("a b c")
     >>> easy_prove(Implies(TruncDiv(a, b) == c, Abs(b) * Abs(c) <= Abs(a)))
--- tests/test_z4.py
+++ tests/test_z4.py
@@ -1,13 +1,17 @@
+# ruff: noqa: D100, D101, D102, D104, INP001, PLR2004, S101
+
 import unittest
 from fractions import Fraction
+
+import pytest
 
 from z4 import (
     ByteVec,
     Int,
     Real,
-    Z3Unknown,
-    Z3Unsat,
-    Z3Unbounded,
+    Z3UnboundedError,
+    Z3UnknownError,
+    Z3UnsatError,
     easy_solve,
     maximize,
     minimize,
@@ -15,86 +19,86 @@ from z4 import (
 
 
 class Tests(unittest.TestCase):
-    def test_maximize_int(self):
+    def test_maximize_int(self) -> None:
         a = Int("a")
         v, m = maximize(3 * a, [a * 2 <= 5])
-        self.assertEqual(v, 6)
-        self.assertEqual(m[a].as_long(), 2)
+        assert v == 6
+        assert m[a].as_long() == 2
 
-    def test_maximize_real(self):
+    def test_maximize_real(self) -> None:
         a = Real("a")
         v, m = maximize(3 * a, [a * 2 <= 5])
-        self.assertEqual(v, Fraction(15, 2))
-        self.assertEqual(m[a].as_fraction(), Fraction(5, 2))
+        assert v == Fraction(15, 2)
+        assert m[a].as_fraction() == Fraction(5, 2)
 
-    def test_maximize_unbounded_int(self):
+    def test_maximize_unbounded_int(self) -> None:
         a = Int("a")
-        with self.assertRaises(Z3Unbounded):
+        with pytest.raises(Z3UnboundedError):
             maximize(a, [])
 
-    def test_maximize_unbounded_real(self):
+    def test_maximize_unbounded_real(self) -> None:
         a = Real("a")
-        with self.assertRaises(Z3Unbounded):
+        with pytest.raises(Z3UnboundedError):
             maximize(a, [])
 
-    def test_maximize_no_solution(self):
+    def test_maximize_no_solution(self) -> None:
         a = Int("a")
-        with self.assertRaises(Z3Unsat):
-            maximize(a, [4 < 2 * a, 2 * a < 6])
+        with pytest.raises(Z3UnsatError):
+            maximize(a, [2 * a > 4, 2 * a < 6])
 
-    def test_maximize_unknown(self):
+    def test_maximize_unknown(self) -> None:
         a = Real("a")
-        with self.assertRaises(Z3Unknown):
+        with pytest.raises(Z3UnknownError):
             maximize(a, [2**a == 3])
 
-    def test_minimize_int(self):
+    def test_minimize_int(self) -> None:
         a = Int("a")
         v, m = minimize(3 * a, [a * 2 >= 3])
-        self.assertEqual(v, 6)
-        self.assertEqual(m[a].as_long(), 2)
+        assert v == 6
+        assert m[a].as_long() == 2
 
-    def test_minimize_real(self):
+    def test_minimize_real(self) -> None:
         a = Real("a")
         v, m = minimize(3 * a, [a * 2 >= 3])
-        self.assertEqual(v, Fraction(9, 2))
-        self.assertEqual(m[a].as_fraction(), Fraction(3, 2))
+        assert v == Fraction(9, 2)
+        assert m[a].as_fraction() == Fraction(3, 2)
 
-    def test_minimize_unbounded_int(self):
+    def test_minimize_unbounded_int(self) -> None:
         a = Int("a")
-        with self.assertRaises(Z3Unbounded):
+        with pytest.raises(Z3UnboundedError):
             minimize(a, [])
 
-    def test_minimize_unbounded_real(self):
+    def test_minimize_unbounded_real(self) -> None:
         a = Real("a")
-        with self.assertRaises(Z3Unbounded):
+        with pytest.raises(Z3UnboundedError):
             minimize(a, [])
 
-    def test_minimize_no_solution(self):
+    def test_minimize_no_solution(self) -> None:
         a = Int("a")
-        with self.assertRaises(Z3Unsat):
-            minimize(a, [4 < 2 * a, 2 * a < 6])
+        with pytest.raises(Z3UnsatError):
+            minimize(a, [2 * a > 4, 2 * a < 6])
 
-    def test_minimize_unknown(self):
+    def test_minimize_unknown(self) -> None:
         a = Real("a")
-        with self.assertRaises(Z3Unknown):
+        with pytest.raises(Z3UnknownError):
             minimize(a, [2**a == 3])
 
-    def test_rshift(self):
+    def test_rshift(self) -> None:
         a = ByteVec("a", 1)
         b = ByteVec("b", 1)
         c = ByteVec("c", 1)
         d = ByteVec("d", 1)
 
         m = easy_solve([a == 0xFE, b == (a >> 1), c == 0x2, d == (0xFE >> c)])
-        self.assertEqual(b.value(m), b"\x7f")
-        self.assertEqual(d.value(m), b"\x3f")
+        assert b.value(m) == b"\x7f"
+        assert d.value(m) == b"?"
 
-    def test_byte_vec(self):
+    def test_byte_vec(self) -> None:
         text = ByteVec("text", 3)
         m = easy_solve([text[0] == ord("F"), text[1] == ord("O"), text[2] == ord("O")])
-        self.assertEqual(text.value(m), b"FOO")
+        assert text.value(m) == b"FOO"
 
-    def test_bool_operations(self):
+    def test_bool_operations(self) -> None:
         x = Int("x")
         y = Int("y")
 
@@ -106,8 +110,8 @@ class Tests(unittest.TestCase):
                 == 2
             ]
         )
-        self.assertEqual(m[x], -2)
-        self.assertEqual(m[y], 4)
+        assert m[x] == -2
+        assert m[y] == 4
 
 
 if __name__ == "__main__":
